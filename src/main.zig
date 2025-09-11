b9e8173a1a9eb08b71308192a34ddc2b22fd66d1
Tachera Sasi <tacherasasi@gmail.com>
2025-09-11 08:48:01 +0300

    Add Makefile for build, run, test, and formatting commands

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 const  print = std.debug.print;
 pub fn main() !void {
-    print("Hello, world!\n", .{});
+                print("Hello, world!\n", .{});
     // Prints to stderr, ignoring potential errors.
     print("All your {s} are belong to us.\n", .{"codebase"});
 }

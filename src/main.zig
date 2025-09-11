c15da87968c6ee06f4a5309695833a1d8ab12179
Tachera Sasi <tacherasasi@gmail.com>
2025-09-11 08:46:53 +0300

    Refactor print statements to use a local alias for improved readability

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,8 @@
 const std = @import("std");
-const practice = @import("practice");
 
+const  print = std.debug.print;
 pub fn main() !void {
-    std.debug.print("Hello, world!\n", .{});
+    print("Hello, world!\n", .{});
     // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try practice.bufferedPrint();
+    print("All your {s} are belong to us.\n", .{"codebase"});
 }

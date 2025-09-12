5e8261210bc3bf42be21f96c5aa6b563c98ef986
Tachera Sasi <tacherasasi@gmail.com>
2025-09-13 01:21:00 +0300

    Add User struct and update main function to print user details

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,17 @@
 const std = @import("std");
 
+const User = struct {
+    name: []const u8,
+    age: i32
+};
 const print = std.debug.print;
 pub fn main() !void {
+    const user = User{
+        .name = "Alice",
+        .age = 30,
+    };
+    print(user);
+    print("User: {s}, Age: {d}\n", .{user.name, user.age});
     print("Hello, world!\n", .{});
     // Prints to stderr, ignoring potential errors.
     print("All your {s} are belong to us.\n", .{"codebase"});

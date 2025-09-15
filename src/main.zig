7e481df7dd0d630ee4fea60eb8792b5ef72d405d
Tachera Sasi <tacherasasi@gmail.com>
2025-09-15 22:50:31 +0300

    Add error handling for invalid user age in main function

--- src/main.zig
+++ src/main.zig
@@ -4,12 +4,21 @@ const User = struct {
     name: []const u8,
     age: i32
 };
+const CustomError = error{InvalidAge, UnrealisticAge};
 const print = std.debug.print;
 pub fn main() !void {
     const user = User{
         .name = "Alice",
-        .age = 30,
+        .age = -1,
     };
+    if (user.age < 0){
+        print("Error: Invalid age {d}\n", .{user.age});
+        return CustomError.InvalidAge;
+    }else if (user.age > 120) {
+        print("Error: Unrealistic age {d}\n", .{user.age});
+        return CustomError.UnrealisticAge;
+    }
+
     print("{}", .{user});
     print("User: {s}, Age: {d}\n", .{user.name, user.age});
     print("Hello, world!\n", .{});

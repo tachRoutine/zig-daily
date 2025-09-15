530c6f541a8de42290dcf3419c12adb4d639e410
Tachera Sasi <tacherasasi@gmail.com>
2025-09-15 22:55:34 +0300

    Refactor error handling and improve code formatting in main function

--- src/main.zig
+++ src/main.zig
@@ -1,28 +1,25 @@
 const std = @import("std");
 
-const User = struct {
-    name: []const u8,
-    age: i32
-};
-const CustomError = error{InvalidAge, UnrealisticAge};
-const print = std.debug.print;
+const User = struct { name: []const u8, age: i32 };
 
+const CustomError = error{ InvalidAge, UnrealisticAge, HIIIIIIIII };
+const print = std.debug.print;
 
 pub fn main() !void {
     const user = User{
         .name = "Alice",
         .age = -1,
     };
-    if (user.age < 0){
+    if (user.age < 0) {
         print("Error: Invalid age {d}\n", .{user.age});
-        return CustomError.InvalidAge;
-    }else if (user.age > 120) {
+        return CustomError.HIIIIIIIII;
+    } else if (user.age > 120) {
         print("Error: Unrealistic age {d}\n", .{user.age});
         return CustomError.UnrealisticAge;
     }
 
     print("{}", .{user});
-    print("User: {s}, Age: {d}\n", .{user.name, user.age});
+    print("User: {s}, Age: {d}\n", .{ user.name, user.age });
     print("Hello, world!\n", .{});
     // Prints to stderr, ignoring potential errors.
     print("All your {s} are belong to us.\n", .{"codebase"});

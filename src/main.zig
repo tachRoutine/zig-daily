9a2ab0720de609253088a04a23dd1decaea7f2f9
Tachera Sasi <tacherasasi@gmail.com>
2025-09-17 13:30:48 +0300

    Update user age in main function to a valid value and add panic for unrealistic age

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,7 @@ const User = struct {
 pub fn main() !void {
     var user = User{
         .name = "Alice",
-        .age = -1,
+        .age = 100000,
     };
     user.greetUser();
     if (user.age < 0) {
@@ -20,6 +20,7 @@ pub fn main() !void {
         return CustomError.HIIIIIIIII;
     } else if (user.age > 120) {
         print("Error: Unrealistic age {d}\n", .{user.age});
+        std.debug.panic("Common man");
         return CustomError.UnrealisticAge;
     }
 
